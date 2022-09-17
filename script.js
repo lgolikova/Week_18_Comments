8c941e620397c6ccdd84e5b3e01b3e06c4b33c30
lgolikova <milagoul@gmail.com>
2022-09-17 19:28:18 +0300

    avatar

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ document.querySelector('#btn').addEventListener('click', () => {
     let comment = document.querySelector('#comment'); 
     let commentChecked = comment.value.replace(/xxx|viagra/ig, "***");
     let newComment = document.createElement("div");
-
     let name = document.getElementById('username');
-    let img = document.getElementById('img');
+    // let img = document.getElementById('img');
     let avatar = document.getElementById('avatar');
-    // img.src = avatar.src;
-
-    // console.log(avatar.src);
-
-    newComment.innerHTML = `${img} ${name.value}: ${commentChecked} <br><br>`;
+    console
+    
+    newComment.innerHTML = `${name.value}: ${commentChecked} <br><br>`;
+    
+    let x = document.createElement("img");
+    if(!avatar.value) {
+    x.src = './anonymous.png';
+    x.width='50';
+    x.height='50';
+    } else {
+        x.src = avatar.value;
+    }
+    document.querySelector('.comments').append(x);
     document.querySelector('.comments').append(newComment);
 
-    
 
 
 
